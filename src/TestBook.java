5e0fe01300d9a52d455a3a0a4ae07f43bc6a34a9
Bartosz Galinski <galinski.bartosz@gmail.com>
2019-07-09 19:34:17 +0200

    Add 2nd object and show it.

--- src/TestBook.java
+++ src/TestBook.java
@@ -1,12 +1,19 @@
 public class TestBook {
     public static void main(String[] args) {
-        Book book1 = new Book("Zaginiony symbol", "Dan Brown", 568);
-//        book1.name = "Zaginiony symbol";
-//        book1.author ="Dan Brown";
-//        book1.pages = 568;
+        Book book1 = new Book();
+        Book book2 = new Book();
+        book1.name = "Zaginiony symbol";
+        book1.author ="Dan Brown";
+        book1.pages = 568;
+        book2.name = "Wirus";
+        book2.author = "G. Del Toro";
+        book2.pages = 346;
 
         System.out.println("Tytuł ksiązki: " + book1.name );
         System.out.println("Autor ksiązki: " + book1.author );
         System.out.println("Ilość stron: " + book1.pages );
+        System.out.println("Tytuł ksiązki: " + book2.name );
+        System.out.println("Autor ksiązki: " + book2.author );
+        System.out.println("Ilość stron: " + book2.pages );
     }
 }
